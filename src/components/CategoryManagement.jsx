b3904f0dcb7486da3e0b18ec73f6e1049824d152
Tharindurajasinghe <tharinduarunajith@gmail.com>
2026-01-17 11:57:48 +0530

    catagory box fixed

--- src/components/CategoryManagement.jsx
+++ src/components/CategoryManagement.jsx
@@ -71,7 +71,7 @@ const CategoryManagement = ({ show, onClose, onCategoryChange }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-md">
+      <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-md max-h-[95vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-xl font-bold">Manage Categories</h2>
           <button
